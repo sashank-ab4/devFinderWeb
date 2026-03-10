fdd0654411499190ce4bd11f1d5d82dd8c8a2dff
Sashank <sashankakkabattula@gmail.com>
2026-03-10 16:13:46 +0530

    Added basic routes using react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
-import Navbar from "./components/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import MainBody from "./layout/MainBody";
 
 function App() {
   return (
-    <div>
-      <Navbar />
-    </div>
+    <>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<MainBody />}>
+            <Route path="/app" element={<div>app</div>} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
