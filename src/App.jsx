8408e4cc1df926a15db6603d9f7bb5b515252c4c
Sashank <sashankakkabattula@gmail.com>
2026-03-11 11:03:38 +0530

    Installed react-redux and RTK, created a store, provided to all pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import MainBody from "./layout/MainBody";
+import Login from "./components/LoginPage";
+import { Provider } from "react-redux";
+import appStore from "./utils/appStore";
 
 function App() {
   return (
     <>
-      <BrowserRouter basename="/">
-        <Routes>
-          <Route path="/" element={<MainBody />}>
-            <Route path="/app" element={<div>app</div>} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <Provider store={appStore}>
+        <BrowserRouter basename="/">
+          <Routes>
+            <Route path="/" element={<MainBody />}>
+              <Route path="/login" element={<Login />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </Provider>
     </>
   );
 }
