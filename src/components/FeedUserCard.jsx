604476c2919e85bc97799727d40c6a57b677ff9b
Sashank <sashankakkabattula@gmail.com>
2026-03-12 17:21:20 +0530

    Created a new User card with API data

--- src/components/FeedUserCard.jsx
+++ src/components/FeedUserCard.jsx
@@ -1,17 +1,39 @@
 export default function FeedUserCard({ user }) {
   const { firstName, lastName, age, about, gender } = user;
   return (
-    <div className="card bg-base-200 w-96 shadow-sm">
-      <figure className="px-10 pt-10">
-        <img src={user.photoUrl} alt="Photo" className="rounded-xl" />
-      </figure>
-      <div className="card-body items-center text-center">
-        <h2 className="card-title">{firstName + " " + lastName}</h2>
-        {age && gender && <p>{age + ", " + gender}</p>}
-        <p>{about}</p>
-        <div className="card-actions mt-2">
-          <button className="btn btn-primary">Ignore</button>
-          <button className="btn btn-secondary">Interested</button>
+    <div className="bg-white rounded-xl shadow-lg overflow-hidden border">
+      <div className="h-60 w-full bg-gray-100 flex items-center justify-center overflow-hidden">
+        <img
+          src={
+            user.photoUrl ||
+            "https://cdn-icons-png.flaticon.com/512/149/149071.png"
+          }
+          alt="profile"
+          className="h-full w-full object-contain"
+        />
+      </div>
+
+      <div className="p-6 text-center space-y-3">
+        <h2 className="text-xl font-semibold">
+          {firstName} {lastName}
+        </h2>
+
+        {user.age && user.gender && (
+          <p className="text-gray-500">
+            {age}, {gender}
+          </p>
+        )}
+
+        <p className="text-gray-600">{about}</p>
+
+        <div className="flex justify-center gap-4 pt-3">
+          <button className="px-4 py-2 rounded-lg bg-gray-200 hover:bg-gray-300">
+            Ignore
+          </button>
+
+          <button className="px-4 py-2 rounded-lg bg-blue-600 text-white hover:bg-blue-700">
+            Interested
+          </button>
         </div>
       </div>
     </div>
