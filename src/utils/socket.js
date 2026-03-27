90648adb4b5716811580ced2a1be75d90d2a4757
Sashank <sashankakkabattula@gmail.com>
2026-03-27 16:02:28 +0530

    bug fix

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -3,5 +3,6 @@ import { BASE_BACKEND_URL } from "./mockData";
 export const createSocketConnection = () => {
   return io(BASE_BACKEND_URL, {
     withCredentials: true,
+    transports: ["websocket", "polling"],
   });
 };
